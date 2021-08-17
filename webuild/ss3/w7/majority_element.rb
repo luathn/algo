30d47f2246900c04718b854831057b5c4707273e
luathn <luat1203@gmail.com>
2021-08-17 10:38:09 +0700

    Webuild ss3 w7: Update better algo

--- webuild/ss3/w7/majority_element.rb
+++ webuild/ss3/w7/majority_element.rb
@@ -1,14 +1,32 @@
+# https://leetcode.com/problems/majority-element/
+
+# def majority_element(nums)
+#   len = nums.length
+#   hash = {}
+#   nums.each do |n|
+#     if hash.include?(n)
+#       hash[n] += 1
+#     else
+#       hash[n] = 1
+#     end
+#     return n if hash[n] > len / 2
+#   end
+# end
+
 def majority_element(nums)
-  len = nums.length
-  hash = {}
+  majority_num = nums[0]
+  count = 0
   nums.each do |n|
-    if hash.include?(n)
-      hash[n] += 1
+    if n == majority_num
+      count += 1
+    elsif count.zero?
+      majority_num = n
+      count += 1
     else
-      hash[n] = 1
+      count -= 1
     end
-    return n if hash[n] > len / 2
   end
+  majority_num
 end
 
 nums = [2, 2, 1, 1, 1, 2, 2]
